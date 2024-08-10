784fb2d0b17c26eb23e73e6dcea1d77d919fedd8
darshan <panchaldarshan507@gmail.com>
2024-08-10 17:10:08 +1200

    updated project

--- src/app/pages/projects/page.js
+++ src/app/pages/projects/page.js
@@ -4,7 +4,18 @@ import styles from './project.module.css';
 
 const page = () => {
   return (
-   <></>
+   <div className={styles.projectMain}>
+       <div className={styles.head}>
+<div className={styles.headJR}>    
+      <h2>Projects</h2>
+</div>
+
+       </div>
+       <div className={styles.jigu}><hr/></div>
+
+       <div className={styles.projectLoad}><h2>Uploading soon ...</h2></div>
+
+   </div>
   )
 }
 
